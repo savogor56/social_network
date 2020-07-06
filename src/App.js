e34d8271e660132015630a40a17bcfe152240506
BiGRUSSIANHYPE <BiGRUSSIANHYPE>
2020-07-06 13:24:31 +0700

    raise data

--- src/App.js
+++ src/App.js
@@ -6,75 +6,16 @@ import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
 import { Route, BrowserRouter } from 'react-router-dom';
 
-let users = {
-  user1: {
-    name: "Vasya",
-    avatar: "https://s3.amazonaws.com/liberty-uploads/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg",
-    email: "Vasya228@mail.ru",
-    description: "It is Vasiliy"
-  },
-  user2: {
-    name: "Pasha",
-    avatar: "https://www.meme-arsenal.com/memes/b5d2ec8e1ffa887b239fb66a8653dfe6.jpg",
-    email: "Paul345@mail.ru",
-    description: "Its Pasha"
-  }
-}
 
-let dialogsData= [
-  {
-    name: "Andrey",
-    id: 1
-  },
-  {
-    name: "Pasha",
-    id: 2
-  },
-  {
-    name: "Ivan",
-    id: 3
-  }
-]
 
-let messagesData = [
-  {
-    id: 1,
-    message: "Hi"
-  },
-  {
-    id: 2,
-    message: "what's up man?"
-  },
-  {
-    id: 3,
-    message: "By"
-  }
-]
-
-let postsData = [
-  {
-    id: 1,
-    message: "Hi bro, its my first post!! How are u?",
-    likesCount: 2,
-    avatar: users.user2.avatar
-  },
-  {
-    id: 2,
-    message: "Hi and me too! I'm ok!",
-    likesCount: 1,
-    avatar: users.user1.avatar
-  }
-
-]
-
-const App = () => {
+const App = (props) => {
   return (
     <BrowserRouter>    
       <div className="app_wrapper">      
         <Header />  
         <Aside />
-        <Route path="/profile" render={() => <Profile users={users} postsData={postsData} /> }/>
-        <Route exact path="/dialogs" render={()=> <Dialogs messagesData={messagesData} dialogsData={dialogsData} />} />
+        <Route path="/profile" render={() => <Profile users={props.users} postsData={props.postsData} /> }/>
+        <Route exact path="/dialogs" render={()=> <Dialogs messagesData={props.messagesData} dialogsData={props.dialogsData} />} />
       </div>
     </BrowserRouter>
   );
--- src/index.js
+++ src/index.js
@@ -4,9 +4,70 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+let users = {
+  user1: {
+    name: "Vasya",
+    avatar: "https://s3.amazonaws.com/liberty-uploads/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg",
+    email: "Vasya228@mail.ru",
+    description: "It is Vasiliy"
+  },
+  user2: {
+    name: "Pasha",
+    avatar: "https://www.meme-arsenal.com/memes/b5d2ec8e1ffa887b239fb66a8653dfe6.jpg",
+    email: "Paul345@mail.ru",
+    description: "Its Pasha"
+  }
+}
+
+let dialogsData= [
+  {
+    name: "Andrey",
+    id: 1
+  },
+  {
+    name: "Pasha",
+    id: 2
+  },
+  {
+    name: "Ivan",
+    id: 3
+  }
+]
+
+let messagesData = [
+  {
+    id: 1,
+    message: "Hi"
+  },
+  {
+    id: 2,
+    message: "what's up man?"
+  },
+  {
+    id: 3,
+    message: "By"
+  }
+]
+
+let postsData = [
+  {
+    id: 1,
+    message: "Hi bro, its my first post!! How are u?",
+    likesCount: 2,
+    avatar: users.user2.avatar
+  },
+  {
+    id: 2,
+    message: "Hi and me too! I'm ok!",
+    likesCount: 1,
+    avatar: users.user1.avatar
+  }
+
+]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App users={users} dialogsData={dialogsData} messagesData={messagesData} postsData={postsData} />
   </React.StrictMode>,
   document.getElementById('root')
 );
