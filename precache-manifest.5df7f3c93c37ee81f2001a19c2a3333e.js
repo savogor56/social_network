429a0cdcebd05103a3caf1d8ea7680c8fc0cb852
Кристина Ким <savogor56@gmail.com>
2020-08-23 14:36:26 +0700

    Updates

---
+++ precache-manifest.5df7f3c93c37ee81f2001a19c2a3333e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73884f36d8391716f5c76069ede414d",
+    "url": "/social_network/index.html"
+  },
+  {
+    "revision": "0fa110daa6d9ac8ffc4a",
+    "url": "/social_network/static/css/3.67ab765d.chunk.css"
+  },
+  {
+    "revision": "1a0a46c06b07cce1f760",
+    "url": "/social_network/static/css/main.81c0fdca.chunk.css"
+  },
+  {
+    "revision": "839995039e092c9aa46d",
+    "url": "/social_network/static/js/2.112e7914.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social_network/static/js/2.112e7914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa110daa6d9ac8ffc4a",
+    "url": "/social_network/static/js/3.4eafd14b.chunk.js"
+  },
+  {
+    "revision": "1a0a46c06b07cce1f760",
+    "url": "/social_network/static/js/main.50a2c95a.chunk.js"
+  },
+  {
+    "revision": "6fec68908734d888a95d",
+    "url": "/social_network/static/js/runtime-main.3a5328fe.js"
+  },
+  {
+    "revision": "c4c97976da063c5c797cc2c74672db63",
+    "url": "/social_network/static/media/background.c4c97976.jpg"
+  },
+  {
+    "revision": "edcd29e70cadc58c510123cf3b33da15",
+    "url": "/social_network/static/media/default_avatar.edcd29e7.jpg"
+  },
+  {
+    "revision": "4ab860b2e26f2507d26e3d8fe96a4149",
+    "url": "/social_network/static/media/find_work.4ab860b2.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/social_network/static/media/logo.5d5d9eef.svg"
+  }
+]);
